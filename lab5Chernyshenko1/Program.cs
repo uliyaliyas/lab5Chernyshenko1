90b9a37cbfc1c6b9973d64ec19f12a7565847669
uliyalyaaas <uliyaliya.1304@gmail.com>
2023-11-25 23:14:21 +0200

    Добавьте файлы проекта.

---
+++ lab5Chernyshenko1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите массив:");
+char[] mas=Console.ReadLine().ToArray();
+int max = 0;
+int current = 0;
+foreach (char c in mas)
+{
+    if (c =='a')
+    {
+        current++;
+        if (current > max)
+        {
+            max = current;
+        }
+    }
+    else 
+    {
+        current = 0;
+    }
+}
+Console.WriteLine(max);
